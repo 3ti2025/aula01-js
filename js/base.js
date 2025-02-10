4712eab3df2cbae0105f5c593fe201e1641514ee
Israel <3ti2025@gmail.com>
2025-02-10 11:25:24 -0300

    03-alteração no JS

--- js/base.js
+++ js/base.js
@@ -24,3 +24,7 @@ console.log(`Turma do aluno: ${turmaAluno}`);
 turmaAluno = '3AC';
 console.log(`Turma do aluno: ${turmaAluno}`);
 
+
+console.log('Alterando o arquivo JS, executando um novo commit');
+
+
